33526f6b2bd3b2774db95093c65ebb8fd13026b9
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2017-09-03 16:52:06 -0700

    Added hall effect sensor readings

--- bike/bike.ino
+++ bike/bike.ino
@@ -11,25 +11,58 @@
 
 int a_pot_pin = A0;
 int b_pot_pin = A1;
-
+int hfx_pin = 2;
+int inc = 0;
+int up_count = 0;
+int down_acc = 1;
+int prev_inc = 0;
 int current_volume = 0;
 
 // Run on program initialization
 void setup() {
   Wire.begin();
-  
   initializeTPA6130();
   Serial.begin(9600);
+  pinMode(hfx_pin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(hfx_pin), onRise, RISING);
 }
 
 // Executed on loop
 void loop() {  
-  double a_value = analogRead(a_pot_pin);
-  Serial.print("A: ");
-  Serial.println(a_value);
-  int target_volume = (int) ((1020 - a_value) * 63 / 1020);
-  Serial.print("Volume: " );
-  Serial.println(target_volume);
+  //double a_value = analogRead(a_pot_pin);
+  //Serial.print("A: ");
+  //Serial.println(a_value);
+  //int target_volume = (int) ((1020 - a_value) * 63 / 1020);
+  //Serial.print("Volume: " );
+  //Serial.println(target_volume);
+  delay(10);
+  up_count++;
+  if(up_count == 50){
+    Serial.println("Count reset to 0");
+    up_count = 0;
+    if(inc > 0){
+      if(prev_inc == inc && down_acc < 5) {
+        down_acc++;
+      }
+      else {
+        down_acc = 1;
+      }
+
+      double drop_multiplier = inc / 5.0;
+      
+      inc -= (int) (down_acc * drop_multiplier);
+      if(inc < 0) {
+        inc = 0;
+      }
+      Serial.print("Inc decreased to: ");
+      Serial.println(inc);
+    }
+    prev_inc = inc;
+  }
+  int target_volume = 2 * inc;
+  if(target_volume > 63) {
+    target_volume = 63;
+  }
   setVolume(target_volume);
 }
 
@@ -61,5 +94,9 @@ void mute() {
   Wire.endTransmission();
 }
 
-
+void onRise() {
+  inc++;
+  Serial.print("Inc increased to: ");
+  Serial.println(inc);
+}
 
